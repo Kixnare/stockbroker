d2f94d8d8f009ee85c585a6b6ae7b837498857b5
Piotr Doliasz <piotrdoliasz@gmail.com>
2021-05-01 00:34:24 +0200

    Added new form where you can choose currency in which you invest.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
-let plnElement = document.querySelector(".js-pln");
 let formElement = document.querySelector(".js-form");
-let gmeElement = document.querySelector(".js-gme");
-let pltrElement = document.querySelector(".js-pltr");
-let amcElement = document.querySelector(".js-amc");
-let bbElement = document.querySelector(".js-bb");
-let usd = 3.8;
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let gmeElement = document.querySelector(".js-gmeResult");
+let pltrElement = document.querySelector(".js-pltrResult");
+let amcElement = document.querySelector(".js-amcResult");
+let bbElement = document.querySelector(".js-bbResult");
+
+
+
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let pln = plnElement.value;
+  let ratePLNtoUSD = 0.26;
+  let rateEURtoUSD = 1.20;
+  let gmePriceInUSD = 173.59;
+  let pltrPriceInUSD = 23.04;
+  let amcPriceInUSD = 10.03;
+  let bbPriceInUSD = 8.83;
 
-  let gme = pln / (156.44 * usd);
-  let pltr = pln / (23.15 * usd);
-  let amc = pln / (9.94 * usd);
-  let bb = pln / (8.77 * usd);
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
+
+  switch (currency) {
+    case "PLN":
+      gme = amount * ratePLNtoUSD / gmePriceInUSD
+      pltr = amount * ratePLNtoUSD / pltrPriceInUSD
+      amc = amount * ratePLNtoUSD / amcPriceInUSD
+      bb = amount * ratePLNtoUSD / bbPriceInUSD
+      break;
+    case "USD":
+      gme = amount / gmePriceInUSD
+      pltr = amount / pltrPriceInUSD
+      amc = amount / amcPriceInUSD
+      bb = amount / bbPriceInUSD
+      break;
+    case "EUR":
+      gme = amount * rateEURtoUSD / gmePriceInUSD
+      pltr = amount * rateEURtoUSD / pltrPriceInUSD
+      amc = amount * rateEURtoUSD / amcPriceInUSD
+      bb = amount * rateEURtoUSD / bbPriceInUSD
+      break;
+  }
 
   gmeElement.innerText = gme.toFixed(4);
   pltrElement.innerText = pltr.toFixed(4);
